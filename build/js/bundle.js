bc9b83be1ebafdc06cfe239a55e6e1f0283cce87
Yuriy Bevov <jury19y@mail.ru>
2021-10-15 18:14:23 +0300

    body scroll

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -5076,18 +5076,21 @@ const btns = document.querySelectorAll('.js-modal-opener');
 const onClickShowModal = (evt) => {
     evt.preventDefault();
     evt.stopPropagation();
-    
+
+    const body = document.querySelector('body');
     let modalName = evt.currentTarget.getAttribute("data-id");
     let modal = document.querySelector("." + modalName);
     let content = document.querySelector("." + modalName + "__wrapper");
 
     modal.classList.remove('closed');
+    body.style.overflow = 'hidden';
 
     const onClickCloseModal = (evt) => {
         const clickArea = evt.target == content || content.contains(evt.target);
 
         if(!clickArea) {
             modal.classList.add('closed');
+            body.style.overflow = 'scroll';
             window.removeEventListener('click', onClickCloseModal);
         }
     }
